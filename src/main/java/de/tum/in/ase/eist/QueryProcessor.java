2cae32ffd825e2a5f2f96f3c30a5c9e6eff8a804
Mara Geisler <mara.geisler@tum.de>
2022-07-14 15:01:40 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,9 +12,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Mara2";
+           return "mara2";
         } else { // TODO extend the programm here
-            return "Mara2";
+            return "";
         }
     }
 }
